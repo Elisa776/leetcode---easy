51163c462a6c5f517b5fd3afc2cdc9de1442a88b
Elisa Kolipaka <elisakolipaka@gmail.com>
2025-05-16 20:44:53 +0530

    Update Main.java
    Deleted content of Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,1 @@
-import java.util.*;
-public class main
-{
-    public static void main(String[] args)
-    {
-       Scanner sc = new Scanner(System.in);
-       
-       System.out.print("x = ");
 
-       int x = sc.nextInt();
-
-       Solution a = new Solution();
- 
-        boolean result = a.isPalindrome(x);
-        System.out.print(result);
-       
-    }
-}
